554424dd085f3baaf24312d823b76f0f2b9d5b53
Matthieu Beauche <mathieu.beauche@gmail.com>
2017-02-11 16:48:42 -0500

    - added comments to the code - remembering previous context + intent so the User is not starting from the root node everytime

--- start.js
+++ start.js
@@ -3,6 +3,7 @@
 
 // loads the .env file and sets Constants
 require('dotenv').config();
+var _ = require('underscore');
 
 const watson = require('watson-developer-cloud');
 
@@ -13,16 +14,29 @@ const watson_conversation = new watson.ConversationV1({
   version_date: watson.ConversationV1.VERSION_DATE_2016_09_20
 });
 
-var RtmClient = require('@slack/client').RtmClient;
-var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
-var bot_token = process.env.SLACK_BOT_TOKEN;
-var rtm = new RtmClient(bot_token);
-var WatsonRequestHandler = require('./lib/watson_request_handler');
+const RtmClient = require('@slack/client').RtmClient;
+const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+const rtm = new RtmClient(process.env.SLACK_BOT_TOKEN);
+const WatsonRequestHandler = require('./lib/watson_request_handler');
+
+var context_grouped_by_channel_id = {}; // the end-user will have 1 conversation with Watson
+var intent_grouped_by_channel_id = {};
 
 rtm.start();
 
 rtm.on(RTM_EVENTS.MESSAGE, function(message) {
   if (message.type === 'message' && Boolean(message.text)) {
-    new WatsonRequestHandler(watson_conversation, rtm, message).execute();
+    // retrieving previous context and intent, if exists
+    var prev_context = context_grouped_by_channel_id[message.channel];
+    var prev_intent = intent_grouped_by_channel_id[message.channel];
+
+    new WatsonRequestHandler(watson_conversation, rtm, message, prev_context, prev_intent).execute(function(watson_response) {
+      // updating previous context and intent
+      var tmp_hash = {};
+      tmp_hash[message.channel] = watson_response.context;
+      context_grouped_by_channel_id = _.extend(context_grouped_by_channel_id, tmp_hash);
+      tmp_hash[message.channel] = watson_response.intents;
+      intent_grouped_by_channel_id = _.extend(intent_grouped_by_channel_id, tmp_hash);
+    });
   }
 });
